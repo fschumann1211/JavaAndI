0778c4052a569d3dbce66e52c39917c1f11ec384
Frank Schumann <fjahn78@gmail.com>
2015-02-04 21:10:17 +0100

    Initial commit

---
+++ JavaTutorial/src/Kapitel10.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+import java.util.*;
+
+
+/**
+ * @author frank
+ *
+ */
+public class Kapitel10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String inData;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Geben Sie Daten ein: ");
+		inData = scan.nextLine();
+		
+		System.out.println("Sie haben eingegeben: " + inData);
+	}
+
+}
