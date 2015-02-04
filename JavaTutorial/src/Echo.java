857b1523d293924413473e4d4bc2cd5cd0204f99
Frank Schumann <fjahn78@gmail.com>
2015-02-04 21:47:52 +0100

    Add class EchoQuadrat

--- JavaTutorial/src/Echo.java
+++ JavaTutorial/src/Echo.java
@@ -14,7 +14,6 @@ public class Echo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String inData;
 		Scanner scan = new Scanner(System.in);
 		
