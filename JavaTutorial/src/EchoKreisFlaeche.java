80939d56d50dd0cba60d54c93aee8367ffa5021c
Frank Schumann <fjahn78@gmail.com>
2015-02-04 22:11:46 +0100

    Progammieraufgabe 1

---
+++ JavaTutorial/src/EchoKreisFlaeche.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+import java.util.*;
+
+/**
+ * @author frank
+ *
+ */
+public class EchoKreisFlaeche {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int radius;
+		double flaeche;
+		
+		System.out.println("Geben Sie den Radius des Kreises ein: ");
+		radius = scan.nextInt();
+		flaeche = radius * radius * Math.PI;
+		
+		System.out.println("Die Fläche eines Kreises mit einem Radius von " +
+				radius + " beträgt " + flaeche);
+
+	}
+
+}
