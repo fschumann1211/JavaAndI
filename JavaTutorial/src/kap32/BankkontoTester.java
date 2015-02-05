805e606f70a6082edf3fc8b4bc3732860ee4b47a
Frank Schumann <fjahn78@gmail.com>
2015-02-05 20:04:43 +0100

    Test new method

--- JavaTutorial/src/kap32/BankkontoTester.java
+++ JavaTutorial/src/kap32/BankkontoTester.java
@@ -5,7 +5,7 @@ package kap32;
 
 /**
  * @author frank
- * @version 0.32.8
+ * @version 0.32.12
  *
  */
 public class BankkontoTester {
@@ -15,9 +15,13 @@ public class BankkontoTester {
 	 */
 	public static void main(String[] args) {
 		Bankkonto kto1 = new Bankkonto("123", "Bob", 100);
+		Bankkonto kto2 = new Bankkonto( "92a-44-33", "Kathy Emerson", 0 );
 		
 		System.out.println(kto1.kontoNummer + " " + kto1.kontoInhaber +
-				" " + kto1.kontoStand);
+				" " + kto1.getKontostand());
+		
+		System.out.println(kto2.kontoNummer + " " + kto2.kontoInhaber +
+				" " + kto2.getKontostand());
 
 	}
 
