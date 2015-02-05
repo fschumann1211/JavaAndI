a199d3fda3553070b921419636367107e6bb1111
Frank Schumann <fjahn78@gmail.com>
2015-02-05 12:46:58 +0100

    Add calculations
    *   update loop with logc
    *   bump version to 0.17.3

--- JavaTutorial/src/Kap17/AddiereInteger.java
+++ JavaTutorial/src/Kap17/AddiereInteger.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 /**
  * @author frank
- * @version 0.17.0
+ * @version 0.17.3
  *
  */
 public class AddiereInteger {
@@ -15,19 +15,24 @@ public class AddiereInteger {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner( System.in );
 	    int n, summeAlle = 0, summeGerade = 0, summeUngerade = 0;
 
 	    System.out.println( "Geben Sie den Grenzwert ein:" );
 	    n = scan.nextInt();
+	    scan.close();
 
 	    int zaehler = 0;
 
 	    while ( zaehler <= n)
 	    {
-	      summeAlle = summeAlle + zaehler;
-	      
+	    	summeAlle = summeAlle + zaehler;
+	    	if (zaehler%2 == 0){
+	    		summeGerade = summeGerade + zaehler;
+	    	} else {
+	    		summeUngerade = summeUngerade +zaehler;
+	    	}
+	    	zaehler++;
 	    }
 
 	    System.out.print  ( "Summe von allen: " + summeAlle  );
