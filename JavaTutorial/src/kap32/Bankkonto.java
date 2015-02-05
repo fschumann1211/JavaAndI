78e5c42490164cad645ff2e90869ff2cd3683951
Frank Schumann <fjahn78@gmail.com>
2015-02-05 19:45:17 +0100

    Create class Bankkonto

---
+++ JavaTutorial/src/kap32/Bankkonto.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package kap32;
+
+/**
+ * @author frank
+ * @version 0.32.6
+ *
+ */
+public class Bankkonto {
+	//   Felder
+	String kontoNummer;
+	String kontoInhaber;
+	int kontoStand;
+	
+	//   Konstruktoren
+	Bankkonto (String kontoNummer, String kontoInhaber, int kontoStand){
+		this.kontoNummer = kontoNummer;
+		this.kontoInhaber = kontoInhaber;
+		this.kontoStand = kontoStand;
+	}
+	
+	//   Methoden
+
+}
---
+++ JavaTutorial/src/kap32/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author frank
+ *
+ */
+package kap32;
