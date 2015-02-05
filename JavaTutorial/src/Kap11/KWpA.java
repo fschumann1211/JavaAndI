0976fe17eafc7930ad85a8f84a6d3e83d835430b
Frank Schumann <fjahn78@gmail.com>
2015-02-05 09:49:09 +0100

    Add Aufgabe 1

---
+++ JavaTutorial/src/Kap11/KWpA.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Kap11;
+import java.util.*;
+import java.lang.Math;
+
+/**
+ * @author frank
+ *
+ */
+public class KWpA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double kwPreis, kwJahr;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Geben Sie den Preis pro KWh in Cent an: ");
+		kwPreis = scan.nextDouble();
+		
+		System.out.println("Geben Sie den Verbrauch in KWh pro Jahr an: ");
+		kwJahr =scan.nextDouble();
+		
+		System.out.println("Jahreskosten: " + (kwPreis*kwJahr/100));
+
+	}
+
+}
