f423a7cf5e3e8e4967401f18d4b8946f977d049d
Frank Schumann <fjahn78@gmail.com>
2015-02-05 18:57:18 +0100

    Create class MeilenProGallone

---
+++ JavaTutorial/src/kap31/MeilenProGallone.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package kap31;
+
+/**
+ * @author frank
+ * @version 0.31.3
+ *
+ */
+public class MeilenProGallone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Auto auto = new Auto(32456, 32810, 10.6);
+		
+		System.out.println(auto.berechneMPG());
+
+	}
+
+}
