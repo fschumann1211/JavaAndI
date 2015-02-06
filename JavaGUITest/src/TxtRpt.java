32e2eb93b545d7175acf182b5f967fa24eca4393
Frank Schumann <fjahn78@gmail.com>
2015-02-06 19:40:01 +0100

    Initial Feature Commit

---
+++ JavaGUITest/src/TxtRpt.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author frank
+ *
+ */
+public class TxtRpt extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2755410326286949217L;
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
