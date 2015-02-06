7938bfb709f11768c2c3f8a6d5b27f6efd698ef5
Frank Schumann <fjahn78@gmail.com>
2015-02-06 23:16:59 +0100

    Initial feature commit
    Change-Id: Id7dc1cf70b7ffc2996690152905016e7fb1ad881
    Signed-off-by: Frank Schumann <fjahn78@gmail.com>

---
+++ FatCalc/src/FatCalc.java
@@ -0,0 +1,36 @@
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+
+/**
+ * @author frank
+ * @version 0.61.10
+ *
+ */
+@SuppressWarnings("serial")
+public class FatCalc extends JFrame implements ActionListener {
+
+	public FatCalc(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
