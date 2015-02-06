17f5a757e0e8a8d2a7bc47e22e5f5f1ec997b29e
Frank Schumann <fjahn78@gmail.com>
2015-02-06 18:04:28 +0100

    Register ActionListener with btns

--- JavaGUITest/src/TwoBtns.java
+++ JavaGUITest/src/TwoBtns.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -9,7 +10,7 @@ import javax.swing.JFrame;
 
 /**
  * @author frank
- * @version 0.59.4
+ * @version 0.59.7
  *
  */
 public class TwoBtns extends JFrame implements ActionListener {
@@ -18,20 +19,8 @@ public class TwoBtns extends JFrame implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = -6250452776372360461L;
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-	 */
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public TwoBtns(String title){
-		super (title);
-		
-	}
+	private JButton redBtn;
+	private JButton grnBtn;
 
 	/**
 	 * @param args
@@ -41,6 +30,65 @@ public class TwoBtns extends JFrame implements ActionListener {
 		
 		demo.setSize(200, 150);
 		demo.setVisible(true);
+	}
+	
+	/**
+	 * @return the redBtn
+	 */
+	public JButton getRedBtn() {
+		return redBtn;
+	}
+
+	/**
+	 * @param redBtn the redBtn to set
+	 */
+	public void setRedBtn(JButton redBtn) {
+		this.redBtn = redBtn;
+	}
+
+	/**
+	 * @return the grnBtn
+	 */
+	public JButton getGrnBtn() {
+		return grnBtn;
+	}
+
+	/**
+	 * @param grnBtn the grnBtn to set
+	 */
+	public void setGrnBtn(JButton grnBtn) {
+		this.grnBtn = grnBtn;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public TwoBtns(String title){
+		super (title);
+		
+		redBtn = new JButton("Rot");
+		grnBtn = new JButton("Grün");
+		
+		redBtn.addActionListener(this);
+		add(redBtn);
+		
+		grnBtn.addActionListener(this);
+		add(grnBtn);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
