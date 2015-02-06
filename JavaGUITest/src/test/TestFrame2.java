72c37e8caa2780f0f66aa04f3b35aca4d0ffad14
Frank Schumann <fjahn78@gmail.com>
2015-02-06 14:51:22 +0100

    Add MyFrame + Test2

---
+++ JavaGUITest/src/test/TestFrame2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package test;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author frank
+ * @version 0.56.11
+ *
+ */
+public class TestFrame2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MyFrame frame = new MyFrame("Hello!");
+		
+		frame.setVisible(true);
+	}
+
+}
