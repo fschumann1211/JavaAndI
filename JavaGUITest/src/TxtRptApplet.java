3273bfd44962b9b6a4ce2fdc9f53607720c15b34
Frank Schumann <fjahn78@gmail.com>
2015-02-06 20:47:03 +0100

    Applet done
    *   Retyped from Tutorial

--- JavaGUITest/src/TxtRptApplet.java
+++ JavaGUITest/src/TxtRptApplet.java
@@ -1,7 +1,11 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  * 
@@ -9,17 +13,43 @@ import javax.swing.JApplet;
 
 /**
  * @author frank
+ * @version 0.60.16
  *
  */
+@SuppressWarnings("serial")
 public class TxtRptApplet extends JApplet implements ActionListener {
+	JTextField source, desination;
+	JLabel sLabel, dLabel;
+	
+	/* (non-Javadoc)
+	 * @see java.applet.Applet#init()
+	 */
+	public void init(){
+		source = new JTextField(15);
+		desination = new JTextField(15);
+		sLabel = new JLabel("Geben Sie Ihren Namen ein:");
+		dLabel = new JLabel("Hier kommt dann ihr Name:");
+		
+		setLayout(new FlowLayout());
+		
+		add(sLabel);
+		add(source);
+		add(dLabel);
+		add(desination);
+		
+		source.addActionListener(this);
+		this.setBackground(Color.white);
+		
+		setSize(400, 100);
+	}
 
 	/* (non-Javadoc)
 	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
+		desination.setText(source.getText());
+		repaint();
 	}
 
 }
