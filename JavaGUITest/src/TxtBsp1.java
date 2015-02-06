7d930136dad7d7a468efc80fcbaf0586f70b2253
Frank Schumann <fjahn78@gmail.com>
2015-02-06 19:11:35 +0100

    Initial feature Commit

---
+++ JavaGUITest/src/TxtBsp1.java
@@ -0,0 +1,65 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+/**
+ * 
+ */
+
+/**
+ * @author frank
+ * @version 0.60.4
+ *
+ */
+public class TxtBsp1 extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3303381175228576064L;
+	private JTextField text;
+	
+	public TxtBsp1(String title){
+		super(title);
+		
+		text = new JTextField(50);
+		getContentPane().setLayout(new FlowLayout());
+		
+		getContentPane().add(text);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public JTextField getText() {
+		return text;
+	}
+
+	public void setText(JTextField text) {
+		this.text = text;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
