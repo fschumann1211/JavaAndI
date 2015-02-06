1978895abb21cbebe9716dc7b8b31769ba5dc09d
Frank Schumann <fjahn78@gmail.com>
2015-02-06 20:34:31 +0100

    Initial feature Commit

---
+++ JavaGUITest/src/TxtRptApplet.java
@@ -0,0 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
+
+/**
+ * 
+ */
+
+/**
+ * @author frank
+ *
+ */
+public class TxtRptApplet extends JApplet implements ActionListener {
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
