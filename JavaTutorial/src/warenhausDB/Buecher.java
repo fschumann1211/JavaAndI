9833eb3a67222a7c4159ff2a2192f5f6ae0dcb33
Frank Schumann <fjahn78@gmail.com>
2015-02-06 10:57:28 +0100

    Complete Buecher class

--- JavaTutorial/src/warenhausDB/Buecher.java
+++ JavaTutorial/src/warenhausDB/Buecher.java
@@ -5,16 +5,58 @@ package warenhausDB;
 
 /**
  * @author frank
- * @version 0.53.6
+ * @version 0.53.11
  *
  */
 public class Buecher extends Waren implements Besteuerbar {
+	private String author;
+
+	/**
+	 * @param beschreibung
+	 * @param preis
+	 * @param author
+	 */
+	public Buecher(String beschreibung, double preis, String author) {
+		super(beschreibung, preis);
+		this.author = author;
+	}
 
 	/**
 	 * 
 	 */
-	public Buecher() {
-		// TODO Auto-generated constructor stub
+
+	/* (non-Javadoc)
+	 * @see warenhausDB.Besteuerbar#berechneSteuer()
+	 */
+	@Override
+	public double berechneSteuer() {
+		return getPreis()*TAX;
+	}
+
+	/* (non-Javadoc)
+	 * @see warenhausDB.Waren#anzeigen()
+	 */
+	@Override
+	public void anzeigen() {
+		// TODO Scaffold for testing. Delete when done.
+		super.anzeigen();
+		System.out.println("Autor: " + author);
+	}
+
+	/* (non-Javadoc)
+	 * @see warenhausDB.Waren#getPreis()
+	 */
+	@Override
+	public double getPreis() {
+		return super.getPreis();
+	}
+
+	/* (non-Javadoc)
+	 * @see warenhausDB.Waren#setPreis(double)
+	 */
+	@Override
+	public void setPreis(double preis) {
+		super.setPreis(preis);
 	}
 
 }
