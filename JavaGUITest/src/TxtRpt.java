a4dd472aa072482f8e0b59f0470c1979867023a8
Frank Schumann <fjahn78@gmail.com>
2015-02-06 19:59:37 +0100

    Bang some sense into it
    * Add FlowLayout Manager
    * Add Labels and Text Fields
    * Register ActionListener
    * Define Action

--- JavaGUITest/src/TxtRpt.java
+++ JavaGUITest/src/TxtRpt.java
@@ -1,7 +1,10 @@
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  * 
@@ -9,6 +12,7 @@ import javax.swing.JFrame;
 
 /**
  * @author frank
+ * @version 0.60.10
  *
  */
 public class TxtRpt extends JFrame implements ActionListener {
@@ -17,22 +21,105 @@ public class TxtRpt extends JFrame implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 2755410326286949217L;
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	/**
+	 * @return the serialversionuid
 	 */
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	private JLabel inLabel = new JLabel("Geben Sie Ihren Namen ein:");
+	private JTextField inTxt = new JTextField(15);
+	
+	private JLabel outLabel = new JLabel("Hier kommt dann Ihr Name:");
+
+	private JTextField outTxt = new JTextField(15);
+
+	public TxtRpt(String title){
+		super(title);
+		
+		setLayout(new FlowLayout());
+		
+		add(inLabel);
+		add(inTxt);
+		add(outLabel);
+		add(outTxt);
+		
+		inTxt.addActionListener(this);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String name = inTxt.getText();
+		outTxt.setText(name);
+		repaint();
+	}
+
+	/**
+	 * @return the inLabel
+	 */
+	public JLabel getInLabel() {
+		return inLabel;
+	}
+
+	/**
+	 * @return the inTxt
+	 */
+	public JTextField getInTxt() {
+		return inTxt;
+	}
+
+	/**
+	 * @return the outLabel
+	 */
+	public JLabel getOutLabel() {
+		return outLabel;
+	}
+
+	/**
+	 * @return the outTxt
+	 */
+	public JTextField getOutTxt() {
+		return outTxt;
+	}
+
+	/**
+	 * @param inLabel the inLabel to set
+	 */
+	public void setInLabel(JLabel inLabel) {
+		this.inLabel = inLabel;
+	}
+
+	/**
+	 * @param inTxt the inTxt to set
+	 */
+	public void setInTxt(JTextField inTxt) {
+		this.inTxt = inTxt;
+	}
+
+	/**
+	 * @param outLabel the outLabel to set
+	 */
+	public void setOutLabel(JLabel outLabel) {
+		this.outLabel = outLabel;
+	}
+
+	/**
+	 * @param outTxt the outTxt to set
+	 */
+	public void setOutTxt(JTextField outTxt) {
+		this.outTxt = outTxt;
 	}
 
 }
