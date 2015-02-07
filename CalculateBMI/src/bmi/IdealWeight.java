c18eccb435cd2641755cf9311d0e9169a7a500d7
Frank Schumann <fjahn78@gmail.com>
2015-02-07 12:38:37 +0100

    add Button Group gender
    Signed-off-by: Frank Schumann <fjahn78@gmail.com>

--- CalculateBMI/src/bmi/IdealWeight.java
+++ CalculateBMI/src/bmi/IdealWeight.java
@@ -6,13 +6,16 @@ package bmi;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 /**
  * @author frank
- * @version 0.63.3
+ * @version 0.63.4
  *
  */
 @SuppressWarnings("serial")
@@ -21,6 +24,7 @@ public class IdealWeight extends JFrame implements ActionListener {
 	private JRadioButton genderM;
 	private JRadioButton genderF;
 	private ButtonGroup genderGroup;
+	private JPanel genderPanel;
 
 	public IdealWeight(){
 		genderM = new JRadioButton("Male", true);
@@ -29,6 +33,13 @@ public class IdealWeight extends JFrame implements ActionListener {
 		genderGroup = new ButtonGroup();
 		genderGroup.add(genderM);
 		genderGroup.add(genderF);
+		
+		genderPanel = new JPanel();
+		
+		genderPanel.setLayout(new BoxLayout(genderPanel, BoxLayout.Y_AXIS));
+		
+		genderPanel.add(genderM);
+		genderPanel.add(genderF);
 	}
 
 	/* (non-Javadoc)
