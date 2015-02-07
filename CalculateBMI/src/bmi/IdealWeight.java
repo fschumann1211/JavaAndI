55d952c73a91a866e20c2afb50b2f9798f735fc4
Frank Schumann <fjahn78@gmail.com>
2015-02-07 12:57:25 +0100

    Add height group : Java Task: TODO Auto-generated method stub
    Change-Id: Ic9566ab19cb2ce305bd88123ba156401b4240c89
    Task-Url: 
    Signed-off-by: Frank Schumann <fjahn78@gmail.com>

--- CalculateBMI/src/bmi/IdealWeight.java
+++ CalculateBMI/src/bmi/IdealWeight.java
@@ -10,12 +10,13 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 /**
  * @author frank
- * @version 0.63.4
+ * @version 0.63.5
  *
  */
 @SuppressWarnings("serial")
@@ -25,8 +26,18 @@ public class IdealWeight extends JFrame implements ActionListener {
 	private JRadioButton genderF;
 	private ButtonGroup genderGroup;
 	private JPanel genderPanel;
+	private JRadioButton heightA;
+	private JRadioButton heightB;
+	private JRadioButton heightC;
+	private JRadioButton heightD;
+	private JRadioButton heightE;
+	private ButtonGroup heightGroup;
+	private JPanel heightPanel;
 
 	public IdealWeight(){
+		/**
+		 * Gender Group
+		 */
 		genderM = new JRadioButton("Male", true);
 		genderF = new JRadioButton("Female", false);
 		
@@ -38,8 +49,36 @@ public class IdealWeight extends JFrame implements ActionListener {
 		
 		genderPanel.setLayout(new BoxLayout(genderPanel, BoxLayout.Y_AXIS));
 		
+		genderPanel.add(new JLabel("Select your gender"));
 		genderPanel.add(genderM);
 		genderPanel.add(genderF);
+		
+		/**
+		 * Height Group
+		 */
+		heightA = new JRadioButton("60 to 64 inches");
+		heightB = new JRadioButton("64 to 68 inches");
+		heightC = new JRadioButton("68 to 72 inches");
+		heightD = new JRadioButton("72 to 76 inches");
+		heightE = new JRadioButton("76 to 80 inches");
+		
+		heightGroup = new ButtonGroup();
+		heightPanel = new JPanel();
+		
+		heightGroup.add(heightA);
+		heightGroup.add(heightB);
+		heightGroup.add(heightC);
+		heightGroup.add(heightD);
+		heightGroup.add(heightE);
+		
+		heightPanel.setLayout(new BoxLayout(heightPanel, BoxLayout.Y_AXIS));
+		
+		heightPanel.add(new JLabel("Select your height"));
+		heightPanel.add(heightA);
+		heightPanel.add(heightB);
+		heightPanel.add(heightC);
+		heightPanel.add(heightD);
+		heightPanel.add(heightE);
 	}
 
 	/* (non-Javadoc)
