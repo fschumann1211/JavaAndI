08c34cf56c989825b3c36e7e54fd05aa5dff027a
Frank Schumann <fjahn78@gmail.com>
2015-02-09 15:32:27 +0100

    Add recursive drawing method
    Signed-off-by: Frank Schumann <fjahn78@gmail.com>

--- Recursion/src/calc/Snowflake.java
+++ Recursion/src/calc/Snowflake.java
@@ -20,18 +20,32 @@ public class Snowflake extends JApplet {
 		graph = gr;
 		int width = getWidth();
 		int height = getHeight();
+		int min = (height > width)?width:height;
 		
 		gr.setColor(Color.white);
-		gr.fillRect(0, 0, width, height);
+		gr.fillRect(0, 0, width-1, height-1);
 		gr.setColor(Color.blue);
 		
-		drawStar(width/2,height/2,3);
+		drawStar(width/2,height/2,min/3);
 
 	}
 
-	private void drawStar(int i, int j, int k) {
+	private void drawStar(int x, int y, int size) {
 		// TODO Auto-generated method stub
+		int endX, endY;
 		
+		if (size < 2) return;
+		/*
+		 * Six lines radiating from center
+		 */
+		
+		for (int i = 0; i < 6; i++){
+			endX = x + (int)(size*Math.cos((2*Math.PI/6)*i));
+			endY = y - (int)(size*Math.sin((2*Math.PI/6)*i));
+			
+			graph.drawLine(x, y, endX, endY);
+			drawStar(endX, endY, size/3);
+		}
 	}
 
 }
