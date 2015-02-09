f49330062bbb3983b936320e6942ae6c471b5694
Frank Schumann <fjahn78@gmail.com>
2015-02-09 12:20:17 +0100

    : Create GUI
    *   define the Fields
    
    Change-Id: Ifbf1600b057b2ccb4f3a6ffa8b748f750fcf2281
    Task-Url: 
    Signed-off-by: Frank Schumann <fjahn78@gmail.com>

---
+++ Recursion/src/calc/Fibonacci.java
@@ -0,0 +1,55 @@
+package calc;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Fibonacci extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3178025831722430552L;
+	private JPanel contentPane, inJPanel, outJPanel;
+	private Box uiBox;
+	private JLabel inJLabel, outJLabel;
+	private JTextField inField, outField;
+	private JButton doJButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fibonacci frame = new Fibonacci();
+					frame.setTitle("Calculate Fibonacci Number");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Fibonacci() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
